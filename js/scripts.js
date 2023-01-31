82bc74a9b7af64b9903ca9023fb6b36f212732d1
frankjrmv <frankjrmv@gmail.com>
2023-01-30 22:39:59 -0500

    Size update
    Added a loop to display each Pokemon and comment on their size

--- js/scripts.js
+++ js/scripts.js
@@ -8,4 +8,16 @@ let pokemonList = [
 { name: "Leafeon", height: 1, type: ["grass"]},
 { name: "Glaceon", height: 0.8, type: ["ice"]},
 { name: "Sylveon", height: 1, type: ["fairy"]},
+{ name: "Wailord", height: 14.5, type: ["water"]},
 	];
+
+//loop that displays each Pokemon and comments on their size
+for (let i=0; i < pokemonList.length; i++) {
+	if (pokemonList[i].height <= 0.9) {
+		document.write(pokemonList[i].name + " (Height: " + pokemonList[i].height + "m) - Aww, it's so small and cute!" + "<br>");
+	}else if  (pokemonList[i].height >= 1 && pokemonList[i].height <3.9) {
+		document.write(pokemonList[i].name + " (Height:" + pokemonList[i].height + "m) - Nice, that's a medium-sized Pokemon!" + "<br>");
+	}else	{
+		document.write(pokemonList[i].name + "(Height:" + pokemonList[i].height + "m) - Wow, that's big!" + "<br>");
+	}
+};
