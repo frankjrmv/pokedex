205bbd0d35ee5440bd8f4e1687dd4853bd614ec7
frankjrmv <frankjrmv@gmail.com>
2023-01-30 20:52:52 -0500

    First pokedex entries
    added the first 'dex entries with the Eevee-lutions!

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,11 @@
-alert('Hello world');
-
-var favoriteFood='pizza';
-document.write(favoriteFood);
+let pokemonList = [
+{ name: "Eevee", height: 0.3, type: ["normal"]},
+{ name: "Vaporeon", height: 1, type: ["water"]},
+{ name: "Jolteon", height: 0.8, type: ["electric"]},
+{ name: "Flareon", height: 0.9, type: ["fire"]},
+{ name: "Espeon", height: 0.9, type: ["psychic"]},
+{ name: "Umbreon", height: 1, type: ["dark"]},
+{ name: "Leafeon", height: 1, type: ["grass"]},
+{ name: "Glaceon", height: 0.8, type: ["ice"]},
+{ name: "Sylveon", height: 1, type: ["fairy"]},
+	];
