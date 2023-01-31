97581c234aa3be8f4fb4a68c1e8deb8ae704aa82
frankjrmv <frankjrmv@gmail.com>
2023-01-30 22:42:32 -0500

    quick fix - type
    changed type to types

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,14 @@
 let pokemonList = [
-{ name: "Eevee", height: 0.3, type: ["normal"]},
-{ name: "Vaporeon", height: 1, type: ["water"]},
-{ name: "Jolteon", height: 0.8, type: ["electric"]},
-{ name: "Flareon", height: 0.9, type: ["fire"]},
-{ name: "Espeon", height: 0.9, type: ["psychic"]},
-{ name: "Umbreon", height: 1, type: ["dark"]},
-{ name: "Leafeon", height: 1, type: ["grass"]},
-{ name: "Glaceon", height: 0.8, type: ["ice"]},
-{ name: "Sylveon", height: 1, type: ["fairy"]},
-{ name: "Wailord", height: 14.5, type: ["water"]},
+{ name: "Eevee", height: 0.3, types: ["normal"]},
+{ name: "Vaporeon", height: 1, types: ["water"]},
+{ name: "Jolteon", height: 0.8, types: ["electric"]},
+{ name: "Flareon", height: 0.9, types: ["fire"]},
+{ name: "Espeon", height: 0.9, types: ["psychic"]},
+{ name: "Umbreon", height: 1, types: ["dark"]},
+{ name: "Leafeon", height: 1, types: ["grass"]},
+{ name: "Glaceon", height: 0.8, types: ["ice"]},
+{ name: "Sylveon", height: 1, types: ["fairy"]},
+{ name: "Wailord", height: 14.5, types: ["water"]},
 	];
 
 //loop that displays each Pokemon and comments on their size
