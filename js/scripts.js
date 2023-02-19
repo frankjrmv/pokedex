8bddc335f1953515e9afd896988ce77b68b312c6
frankjrmv <frankjrmv@gmail.com>
2023-02-18 22:53:52 -0500

    forEach function
    replaced loop function with a forEach function that displays array and details

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,58 @@
+//array listing some pokemon
 let pokemonList = [
-{ name: "Eevee", height: 0.3, types: ["normal"]},
-{ name: "Vaporeon", height: 1, types: ["water"]},
-{ name: "Jolteon", height: 0.8, types: ["electric"]},
-{ name: "Flareon", height: 0.9, types: ["fire"]},
-{ name: "Espeon", height: 0.9, types: ["psychic"]},
-{ name: "Umbreon", height: 1, types: ["dark"]},
-{ name: "Leafeon", height: 1, types: ["grass"]},
-{ name: "Glaceon", height: 0.8, types: ["ice"]},
-{ name: "Sylveon", height: 1, types: ["fairy"]},
-{ name: "Wailord", height: 14.5, types: ["water"]},
+	{ 
+		name: "Eevee", 
+		height: 0.3, 
+		types: ["normal"]
+	},
+	{ 
+		name: "Vaporeon", 
+		height: 1, 
+		types: ["water"]
+	},
+	{ 
+		name: "Jolteon", 
+		height: 0.8, 
+		types: ["electric"]
+	},
+	{ 
+		name: "Flareon", 
+		height: 0.9, 
+		types: ["fire"]
+	},
+	{ 
+		name: "Espeon", 
+		height: 0.9, 
+		types: ["psychic"]
+	},
+	{ 
+		name: "Umbreon", 
+		height: 1, 
+		types: ["dark"]
+	},
+	{ 
+		name: "Leafeon", 
+		height: 1, 
+		types: ["grass"]
+	},
+	{ 
+		name: "Glaceon", 
+		height: 0.8, 
+		types: ["ice"]
+	},
+	{ 
+		name: "Sylveon", 
+		height: 1, 
+		types: ["Fairy"]
+	},
+	{ 
+		name: "Wailord", 
+		height: 14.5, 
+		types: ["water"]
+	}
 	];
 
-//loop that displays each Pokemon and comments on their size
-for (let i=0; i < pokemonList.length; i++) {
-	if (pokemonList[i].height <= 0.9) {
-		document.write(pokemonList[i].name + " (Height: " + pokemonList[i].height + "m) - Aww, it's so small and cute!" + "<br>");
-	}else if  (pokemonList[i].height >= 1 && pokemonList[i].height <3.9) {
-		document.write(pokemonList[i].name + " (Height:" + pokemonList[i].height + "m) - Nice, that's a medium-sized Pokemon!" + "<br>");
-	}else	{
-		document.write(pokemonList[i].name + "(Height:" + pokemonList[i].height + "m) - Wow, that's big!" + "<br>");
-	}
-};
+//forEach function that calls a loop and displays each Pokemon's details
+pokemonList.forEach(function(pokemon) {
+	console.log(pokemon.name + " is "+ pokemon.height + "m tall and it is a " + pokemon.types + " type!");
+});
